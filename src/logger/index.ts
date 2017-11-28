0ee4f99f9d51c3b406c1ae24518b2f03a4ff8857
Behrooz Kamali <behroozkamali@yahoo.com>
2017-11-27 23:31:34 -0500

    add base logger class

---
+++ src/logger/index.ts
@@ -0,0 +1,8 @@
+import * as Console from 'console';
+
+// TODO: complete this class
+export default class Logger {
+    public static error(message: any) {
+        Console.error(message);
+    }
+}
