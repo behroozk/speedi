974fab7e74af8c14dc008241635607ce1e774d4b
Behrooz Kamali <behroozkamali@yahoo.com>
2017-12-14 14:51:47 -0800

    update cacher class to make it client independent

--- src/cacher/index.ts
+++ src/cacher/index.ts
@@ -1,70 +1,37 @@
-import * as express from 'express';
-import { Authentication } from '../authentication/';
 import { DataStore } from '../data_store/';
 import { Logger } from '../logger/';
-import { ICacherOptions } from './options.interface';
 import { ICachedValue } from "./value.interface";
 
 export class Cacher {
-    public static retrieveOrCache({
-        expire = 60,
-        authBased = false,
-    }: ICacherOptions): express.RequestHandler {
-        return async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
-            let key = `cache_${req.ip}_${req.method}_${req.originalUrl}`;
+    public static async retrieve(key: string): Promise<ICachedValue | null> {
 
-            if (authBased && res.locals.authentication) {
-                const authentication: Authentication = res.locals.authentication;
-                key += '_' + JSON.stringify(authentication.token);
-            }
+        try {
+            const value = await Cacher.dataStore.get(key);
 
-            try {
-                const value = await Cacher.dataStore.get(key);
+            if (!value) {
+                return null;
+            } else {
+                const cache = Cacher.parseCachedResponse(key, value);
 
-                if (!value) {
-                    Cacher.replaceResponseSender(res);
-                    Cacher.addResponseFinishListener(res, key, expire);
-                    next();
-                } else {
-                    const cache = Cacher.parseCachedResponse(key, value);
-
-                    if (cache instanceof Error) {
-                        return next();
-                    }
-
-                    res.set('Content-Type', cache.header).send(cache.body).end();
+                if (cache instanceof Error) {
+                    return null;
                 }
-            } catch (error) {
-                Logger.error(error);
-                next();
+
+                return cache;
             }
-        };
+        } catch (error) {
+            Logger.error(error);
+            return null;
+        }
+    }
+
+    public static async store(key: string, value: ICachedValue, expire: number): Promise<boolean> {
+        // TODO: combine set and expire in one method
+        Cacher.dataStore.set(key, JSON.stringify(value));
+        return Cacher.dataStore.expire(key, expire);
     }
 
     private static dataStore = new DataStore('cache');
-
-    private static replaceResponseSender(res: express.Response): void {
-        const originalSend = res.send.bind(res);
-
-        res.send = function send(body: any) {
-            res.locals.body = body;
-            return originalSend(body);
-        };
-    }
-
-    private static addResponseFinishListener(res: express.Response, key: string, expire: number): void {
-        res.on('finish', () => {
-            if (res.statusCode === 200 && res.locals.body) {
-                const cache: ICachedValue = {
-                    body: res.locals.body,
-                    header: res.get('Content-Type'),
-                };
-
-                Cacher.dataStore.set(key, JSON.stringify(cache));
-                Cacher.dataStore.expire(key, expire);
-            }
-        });
-    }
 
     private static parseCachedResponse(key: string, cahcedValue: string): ICachedValue | Error {
         let parsedCachedValue: ICachedValue;
