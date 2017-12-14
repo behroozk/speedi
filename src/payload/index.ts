4080312e0e5fc1027aa16f62f07d6978bfe73fa1
Behrooz Kamali <behroozkamali@yahoo.com>
2017-12-14 14:55:34 -0800

    replace payload validator class with a client independent one with validate method

---
+++ src/payload/index.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+import * as Joi from 'joi';
+import { RequestError } from '../error/request';
+import { ErrorType } from '../error/type.enum';
+
+export class Payload {
+    public static validate(payload: any, schema: Joi.SchemaMap): any {
+        const result = Joi.validate(payload, schema, { stripUnknown: true });
+
+        if (result.error) {
+            throw new RequestError(ErrorType.BadRequest, 'Invalid payload', result.error.details);
+        }
+
+        return result.value;
+    }
+}
--- src/payload_validator/options.interface.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from 'joi';
-
-export interface IPayloadValidatorOptions {
-    params?: Joi.SchemaMap;
-    body?: Joi.SchemaMap;
-    query?: Joi.SchemaMap;
-}
