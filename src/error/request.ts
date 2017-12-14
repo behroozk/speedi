39f3364bc6e1b3a9477937fecb53be976c6432d1
Behrooz Kamali <behroozkamali@yahoo.com>
2017-12-14 15:02:50 -0800

    replace client specific error handler class with a generalized one

--- src/error/request.ts
+++ src/error/request.ts
@@ -1,10 +1,18 @@
+import { STATUS_CODES } from 'http';
 import { ErrorType } from './type.enum';
 
 export class RequestError extends Error {
     public code: number;
+    public type: ErrorType;
+    public metadata: any;
 
-    constructor(public message: string, public type: ErrorType, public metadata: any = {}) {
-        super(message);
-        this.code = type || 500;
+    constructor(type: ErrorType, message?: string, metadata: any = {}) {
+        const MESSAGE = message || STATUS_CODES[type] || '';
+        super(MESSAGE);
+
+        this.message = MESSAGE;
+        this.type = type;
+        this.metadata = metadata;
+        this.code = type;
     }
 }
