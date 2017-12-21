c84c6e4cdda6124a8d367287db0c4052dd82ac64
Behrooz Kamali <behroozkamali@yahoo.com>
2017-12-21 01:26:05 -0500

    add interfaces and options for server classes

---
+++ src/server/http_options.interface.ts
@@ -0,0 +1,5 @@
+export interface IHttpServerOptions {
+    host: string;
+    port: number;
+    protocol?: 'http' | 'https';
+}
---
+++ src/server/index.interface.ts
@@ -0,0 +1,7 @@
+import { IRouteOptions } from '../route/options.interface';
+
+export interface IServer {
+    start(): Promise<boolean>;
+    stop(): Promise<boolean>;
+    addRoutes(routeObjects: IRouteOptions | IRouteOptions[]): void;
+}
