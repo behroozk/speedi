5dd9c339f778c8e98cbedd7c9a499782f004c786
Behrooz Kamali <behroozkamali@yahoo.com>
2019-01-25 16:34:57 -0500

    refactor data store by removing unnecessary async await

--- src/data_store/index.ts
+++ src/data_store/index.ts
@@ -21,51 +21,51 @@ export class DataStore {
         }
     }
 
-    public async get(key: string): Promise<string | null> {
-        return await this.client.get(this.prefix + key);
+    public get(key: string): Promise<string | null> {
+        return this.client.get(this.prefix + key);
     }
 
-    public async set(key: string, value: string): Promise<string> {
-        return await this.client.set(this.prefix + key, value);
+    public set(key: string, value: string): Promise<string> {
+        return this.client.set(this.prefix + key, value);
     }
 
-    public async del(key: string): Promise<boolean> {
-        return await this.client.del(this.prefix + key);
+    public del(key: string): Promise<boolean> {
+        return this.client.del(this.prefix + key);
     }
 
-    public async expire(key: string, seconds: number): Promise<boolean> {
-        return await this.client.expire(this.prefix + key, seconds);
+    public expire(key: string, seconds: number): Promise<boolean> {
+        return this.client.expire(this.prefix + key, seconds);
     }
 
-    public async ttl(key: string): Promise<number> {
-        return await this.client.ttl(this.prefix + key);
+    public ttl(key: string): Promise<number> {
+        return this.client.ttl(this.prefix + key);
     }
 
-    public async incr(key: string): Promise<number> {
-        return await this.client.incr(this.prefix + key);
+    public incr(key: string): Promise<number> {
+        return this.client.incr(this.prefix + key);
     }
 
-    public async decr(key: string): Promise<number> {
-        return await this.client.decr(this.prefix + key);
+    public decr(key: string): Promise<number> {
+        return this.client.decr(this.prefix + key);
     }
 
-    public async slice(list: string, start: number = 0, end: number = -1): Promise<string[]> {
-        return await this.client.slice(this.prefix + list, start, end);
+    public slice(list: string, start: number = 0, end: number = -1): Promise<string[]> {
+        return this.client.slice(this.prefix + list, start, end);
     }
 
-    public async push(list: string, value: string): Promise<number> {
-        return await this.client.push(this.prefix + list, value);
+    public push(list: string, value: string): Promise<number> {
+        return this.client.push(this.prefix + list, value);
     }
 
-    public async unshift(list: string, value: string): Promise<number> {
-        return await this.client.unshift(this.prefix + list, value);
+    public unshift(list: string, value: string): Promise<number> {
+        return this.client.unshift(this.prefix + list, value);
     }
 
-    public async pop(list: string): Promise<string> {
-        return await this.client.pop(this.prefix + list);
+    public pop(list: string): Promise<string> {
+        return this.client.pop(this.prefix + list);
     }
 
-    public async shift(list: string): Promise<string> {
-        return await this.client.shift(this.prefix + list);
+    public shift(list: string): Promise<string> {
+        return this.client.shift(this.prefix + list);
     }
 }
