550111242687bf96a606b3e388511148922d124f
Behrooz Kamali <behroozkamali@yahoo.com>
2019-01-25 16:35:47 -0500

    refactor cacher and make it functional

--- src/cacher/index.ts
+++ src/cacher/index.ts
@@ -2,54 +2,56 @@ import { DataStore } from '../data_store/';
 import { Logger } from '../logger/';
 import { ICachedValue } from './value.interface';
 
-export class Cacher {
-    public static async retrieve(key: string): Promise<ICachedValue | null> {
-        if (!Cacher.dataStore) {
-            Cacher.dataStore = new DataStore('cache');
+let dataStore: DataStore | undefined;
+
+export async function retrieve(key: string): Promise<ICachedValue | null> {
+    try {
+        if (!dataStore) {
+            dataStore = new DataStore('cache');
         }
 
-        try {
-            const value = await Cacher.dataStore.get(key);
+        const value = await dataStore.get(key);
 
-            if (!value) {
-                return null;
-            } else {
-                const cache = Cacher.parseCachedResponse(key, value);
-
-                if (cache instanceof Error) {
-                    return null;
-                }
-
-                return cache;
-            }
-        } catch (error) {
-            Logger.error(error);
+        if (!value) {
             return null;
+        } else {
+            const cache = parseCachedResponse(key, value);
+
+            if (cache instanceof Error) {
+                return null;
+            }
+
+            return cache;
         }
+    } catch (error) {
+        Logger.error(error);
+        return null;
+    }
+}
+
+export function store(key: string, value: ICachedValue, expire: number): Promise<boolean> {
+    if (!dataStore) {
+        dataStore = new DataStore('cache');
     }
 
-    public static async store(key: string, value: ICachedValue, expire: number): Promise<boolean> {
-        if (!Cacher.dataStore) {
-            Cacher.dataStore = new DataStore('cache');
+    // TODO: combine set and expire in one method
+    dataStore.set(key, JSON.stringify(value));
+    return dataStore.expire(key, expire);
+}
+
+function parseCachedResponse(key: string, cahcedValue: string): ICachedValue | Error {
+    let parsedCachedValue: ICachedValue;
+
+    try {
+        parsedCachedValue = JSON.parse(cahcedValue);
+        return parsedCachedValue;
+    } catch (e) {
+        if (!dataStore) {
+            dataStore = new DataStore('cache');
         }
 
-        // TODO: combine set and expire in one method
-        Cacher.dataStore.set(key, JSON.stringify(value));
-        return Cacher.dataStore.expire(key, expire);
+        dataStore.del(key);
     }
 
-    private static dataStore: DataStore;
-
-    private static parseCachedResponse(key: string, cahcedValue: string): ICachedValue | Error {
-        let parsedCachedValue: ICachedValue;
-
-        try {
-            parsedCachedValue = JSON.parse(cahcedValue);
-            return parsedCachedValue;
-        } catch (e) {
-            Cacher.dataStore.del(key);
-        }
-
-        throw new Error('invalid cache');
-    }
+    throw new Error('invalid cache');
 }
