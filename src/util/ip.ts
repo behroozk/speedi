d739bd9e76f75cf7561ca40e7978ab3aa83efdd8
Behrooz Kamali <behroozkamali@yahoo.com>
2020-12-15 20:58:18 -0800

    Fix bug for undefined network interfaces

--- src/util/ip.ts
+++ src/util/ip.ts
@@ -11,7 +11,7 @@ export class Ip {
             Object.keys(ifaces).forEach((ifname) => {
                 let alias: number = 0;
 
-                ifaces[ifname].forEach((iface) => {
+                (ifaces[ifname] || []).forEach((iface) => {
                     if ('IPv4' !== iface.family || iface.internal !== false) {
                         // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
                         return;
